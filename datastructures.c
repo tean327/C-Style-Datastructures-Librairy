a4e50d70ffe9e0b7fa4c244368f821eafc730021
Grimault Martinez Etan <e.grimault@student.isartdigital.com>
2026-06-24 15:47:11 +0200

    add basic string type hash table

--- datastructures.c
+++ datastructures.c
@@ -73,6 +73,44 @@ typedef struct list_queue_string
 
 #pragma endregion
 
+#pragma region Tree
+typedef struct tree_string
+{
+    char *val;
+    struct tree_string *left;
+    struct tree_string *right;
+} TreeString;
+
+typedef struct tree_int
+{
+    int val;
+    struct tree_int *left;
+    struct tree_int *right;
+} TreeInt;
+#pragma endregion
+
+#pragma region HashTables
+
+typedef struct NodeItem
+{
+    int key;
+    char *data;
+} Item;
+
+typedef struct listHashItem
+{
+    Item *value;
+    struct listHashItem *next;
+} ListItem;
+
+typedef struct NodeHashTable
+{
+    ListItem **hashData;
+    int size;
+    int count;
+} HashTable;
+#pragma endregion
+
 #pragma region List Functions
 void AddAtEndOfListChar(char *value, ListChar *head)
 {
@@ -133,6 +171,7 @@ ListChar *GetListCharNodeFromValue(ListChar *head, char *value)
         {
             return searchNode;
         }
+        searchNode = searchNode->next;
     }
     return NULL;
 }
@@ -207,6 +246,7 @@ ListInt *GetListIntNodeFromValue(ListInt *head, int value)
         {
             return searchNode;
         }
+        searchNode = searchNode->next;
     }
     return NULL;
 }
@@ -387,18 +427,174 @@ int DequeueArrInt(QueueIntArr *queue)
 #pragma endregion
 #pragma region List Based
 // string Queus Funcs
-void EnqueueListStr()
+void EnqueueListStr(QueueStringList *head, char *value)
 {
+    QueueStringList *newNode = (QueueStringList *)malloc(sizeof(QueueStringList));
+    newNode->val = (char *)malloc(sizeof(char) * strlen(value));
+    printf("gre\n");
+    strcpy(newNode->val, value);
+    newNode->next = NULL;
+    if (head->next == NULL)
+    {
+        head->next = newNode;
+        newNode->previous = head;
+        return;
+    }
+
+    QueueStringList *tmp = (QueueStringList *)malloc(sizeof(QueueStringList));
+    tmp = head;
+    while (tmp->next != NULL)
+    {
+        printf("here\n");
+        printf("here%p\n", tmp->next);
+        tmp = tmp->next;
+    }
+    printf("hereeee\n");
+    tmp->next = newNode;
+    newNode->previous = tmp;
+    free(tmp);
 }
-void DequeueListStr()
+QueueStringList *DequeueListStr(QueueStringList *head)
 {
+    QueueStringList *tmp = (QueueStringList *)malloc(sizeof(QueueStringList));
+    QueueStringList *returnNode = (QueueStringList *)malloc(sizeof(QueueStringList));
+    tmp = head;
+    while (tmp->previous != NULL)
+    {
+        tmp = tmp->previous;
+    }
+    returnNode = tmp;
+    tmp->previous->previous = NULL;
+    free(tmp);
+    return returnNode;
 }
 // Int queues Funcs
-void EnqueueListInt()
+void EnqueueListInt(QueueIntList *head, int value)
 {
 }
-void DequeueListInt()
+void DequeueListInt(QueueIntList *head)
 {
 }
 #pragma endregion
+
+void AddAtEndOfListItem(Item *value, ListItem *head)
+{
+    ListItem *newNode = (ListItem *)malloc(sizeof(ListItem));
+    if (newNode == NULL)
+    {
+        printf("Unable to allocate memory for new node\n");
+        // Exit will end the program with the code 1
+        exit(1);
+    }
+    newNode->value = value;
+    newNode->next = NULL;
+
+    ListItem *current = head;
+    while (current->next != NULL)
+    {
+        current = current->next;
+    }
+    current->next = newNode;
+}
+
+/// @brief This function is used to get the node from a list based on a parameter value it can also be used to check if a value is inside a list but it's quite to big for only that
+/// @param head the head of the list
+/// @param value the value you want to check if it's inside
+/// @return Returns the node whose value is equal to the parameter if the value is inside else null if it's not
+ListItem *GetListItemNodeFromValue(ListItem *head, char *value, int key)
+{
+    ListItem *searchNode = head->next;
+    while (searchNode != NULL)
+    {
+        if (strcmp(searchNode->value->data, value) == 0 && searchNode->value->key == key)
+        {
+            return searchNode;
+        }
+        searchNode = searchNode->next;
+    }
+    return NULL;
+}
+
+void FreeListItemMemory(ListItem *head)
+{
+    ListItem *tmp;
+    while (head != NULL)
+    {
+        tmp = head;
+        head = head->next;
+        free(tmp->value->data);
+        free(tmp->value);
+        free(tmp);
+    }
+}
+
+HashTable *CreateHashTable(int tableSize)
+{
+    HashTable *table = (HashTable *)malloc(sizeof(HashTable));
+    if (table == NULL)
+    {
+        printf("Unable to allocate memory for new table\n");
+        exit(1);
+    }
+    table->size = tableSize;
+    table->hashData = malloc(tableSize * sizeof(ListItem *));
+    if (table->hashData == NULL)
+    {
+        printf("Unable to allocate memory for new table data\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < tableSize; i++)
+    {
+        table->hashData[i] = (ListItem *)malloc(sizeof(ListItem));
+        if (table->hashData[i] == NULL)
+        {
+            printf("Unable to allocate memory for new list item\n");
+            exit(1);
+        }
+        table->hashData[i]->next = NULL;
+        table->hashData[i]->value = NULL;
+    }
+    return table;
+}
+
+void DestroyHashTable(HashTable *table)
+{
+    for (int i = 0; i < table->size; i++)
+    {
+        FreeListItemMemory(table->hashData[i]);
+    }
+    free(table->hashData);
+    free(table);
+}
+unsigned int hash(char *str, int hashSize)
+{
+    int sum = 0;
+    for (int i = 0; i < strlen(str); i++)
+    {
+        sum += str[i];
+    }
+    return sum % hashSize;
+}
+
+void InsertIntoHashTable(char *str, HashTable *table)
+{
+    int key = hash(str, table->size);
+    Item *newItem = (Item *)malloc(sizeof(Item));
+    if (newItem == NULL)
+    {
+        printf("Unable to allocate memory for new item\n");
+        exit(1);
+    }
+    newItem->data = strdup(str);
+    newItem->key = key;
+    AddAtEndOfListItem(newItem, table->hashData[key]);
+    table->count++;
+}
+
+ListItem *GetItemInsideTable(HashTable *table, char *str)
+{
+    int key = hash(str, table->size);
+    return GetListItemNodeFromValue(table->hashData[key], str, key);
+}
 #pragma endregion
--- datastructures.h
+++ datastructures.h
@@ -72,6 +72,42 @@ typedef struct list_queue_string
 } QueueStringList;
 
 #pragma endregion
+#pragma region Tree
+typedef struct tree_string
+{
+    char *val;
+    struct tree_string *left;
+    struct tree_string *right;
+} TreeString;
+
+typedef struct tree_int
+{
+    int val;
+    struct tree_int *left;
+    struct tree_int *right;
+} TreeInt;
+#pragma endregion
+
+#pragma region HashTables
+typedef struct NodeItem
+{
+    int key;
+    char *data;
+} Item;
+
+typedef struct listHashItem
+{
+    Item *value;
+    struct listHashItem *next;
+} ListItem;
+
+typedef struct NodeHashTable
+{
+    ListItem **hashData;
+    int size;
+    int count;
+} HashTable;
+#pragma endregion
 
 #pragma region List Functions
 // string List functions
@@ -120,12 +156,24 @@ int DequeueArrInt(QueueIntArr *queue);
 #pragma endregion
 #pragma region List Based
 // string Queus Funcs
-void EnqueueListStr();
-void DequeueListStr();
+void EnqueueListStr(QueueStringList *head, char *value);
+QueueStringList *DequeueListStr(QueueStringList *head);
 // Int queues Funcs
 void EnqueueListInt();
 void DequeueListInt();
 #pragma endregion
+
+#pragma region HashTable
+HashTable *CreateHashTable(int tableSize);
+void DestroyHashTable(HashTable *table);
+unsigned int hash(char *str, int hashSize);
+void InsertIntoHashTable(char *str, HashTable *table);
+void AddAtEndOfListItem(Item *value, ListItem *head);
+ListItem *GetListItemNodeFromValue(ListItem *head, char *value, int key);
+void FreeListItemMemory(ListItem *head);
+ListItem *GetItemInsideTable(HashTable *table, char *str);
+#pragma endregion
+
 #pragma endregion
 
 #endif
